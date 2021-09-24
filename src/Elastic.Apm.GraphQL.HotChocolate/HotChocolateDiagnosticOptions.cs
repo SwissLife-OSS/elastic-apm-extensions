113533e9974bd36314de171b72480c8ab3bd6b90
glucaci <gabriel.lucaci.dev@gmail.com>
2021-09-24 11:39:43 +0200

    Replace delegate with Action

--- src/Elastic.Apm.GraphQL.HotChocolate/HotChocolateDiagnosticOptions.cs
+++ src/Elastic.Apm.GraphQL.HotChocolate/HotChocolateDiagnosticOptions.cs
@@ -1,17 +1,13 @@
+using System;
 using Elastic.Apm.Api;
 
 namespace Elastic.Apm.GraphQL.HotChocolate
 {
-    /// <summary>
-    /// Gives the possibility to enrich transaction data just before <see cref="RequestActivityScope"/> gets disposed.
-    /// </summary>
-    /// <param name="transaction"></param>
-    /// <param name="details"></param>
-    public delegate void EnrichTransaction(ITransaction transaction, OperationDetails details);
-
     public class HotChocolateDiagnosticOptions
     {
-        /// <inheritdoc cref="EnrichTransaction"/>
-        public EnrichTransaction? Enrich { get; set; }
+        /// <summary>
+        /// Gives the possibility to enrich transaction data just before <see cref="RequestActivityScope"/> gets disposed.
+        /// </summary>
+        public Action<ITransaction, OperationDetails>? Enrich { get; set; }
     }
 }
