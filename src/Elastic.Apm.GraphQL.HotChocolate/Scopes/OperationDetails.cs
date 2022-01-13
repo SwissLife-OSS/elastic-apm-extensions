4d019f9bcc804665af37eb64d485e1d0e6b0a432
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2022-01-13 17:10:18 +0100

    Align with HotChocolate.Diagnostics implementation (#20)

--- src/Elastic.Apm.GraphQL.HotChocolate/Scopes/OperationDetails.cs
+++ src/Elastic.Apm.GraphQL.HotChocolate/Scopes/OperationDetails.cs
@@ -1,30 +1,16 @@
-using System.Collections.Immutable;
-
 namespace Elastic.Apm.GraphQL.HotChocolate
 {
     public class OperationDetails
     {
-        internal static readonly OperationDetails Empty = new OperationDetails(
-            "unnamed",
-            "unknown",
-            new[] { "unknown" }.ToImmutableHashSet(),
-            true);
-
         internal OperationDetails(
             string? name,
-            string? rootSelection,
-            IImmutableSet<string> selections,
             bool hasFailed)
         {
             Name = name;
-            RootSelection = rootSelection;
-            Selections = selections;
             HasFailed = hasFailed;
         }
 
         public string? Name { get; }
-        public string? RootSelection { get; }
-        public IImmutableSet<string> Selections { get; }
         public bool HasFailed { get; }
     }
 }
